a82bf57165fff40721ad49788e5ee733ff3d214d
Shivam <sshivam1394@gmail.com>
2026-05-07 12:20:08 +0530

    Fixed Gemini model

--- app/llm_agent.py
+++ app/llm_agent.py
@@ -5,7 +5,7 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 def ask_ai(question):
 
