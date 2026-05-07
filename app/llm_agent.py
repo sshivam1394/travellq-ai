607749c0193f622de079ebefbe91c188c090b021
Shivam <sshivam1394@gmail.com>
2026-05-07 12:35:59 +0530

    Fixed Gemini endpoint

--- app/llm_agent.py
+++ app/llm_agent.py
@@ -25,7 +25,7 @@ def ask_ai(question):
     {question}
     """
 
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key={API_KEY}"
 
     headers = {
         "Content-Type": "application/json"
